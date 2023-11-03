924eace93b41a32a8f97d33ec7369cbc8222bd39
David Carey <dlcarey88@gmail.com>
2023-11-04 09:31:20 +1100

    Update detect.py

--- detect.py
+++ detect.py
@@ -173,6 +173,7 @@ def run(
                         label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
                         annotator.box_label(xyxy, label, color=colors(c, True))
                     if save_crop:
+                        print(save_dir / 'crops' / names[c] / f'{p.stem}.jpg')
                         save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
 
             # Stream results
