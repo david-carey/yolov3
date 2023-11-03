7d21a370ccd9e2a1efcfefa07eecfbacb34ca469
David Carey <dlcarey88@gmail.com>
2023-11-04 10:04:44 +1100

    Update detect.py

--- detect.py
+++ detect.py
@@ -81,6 +81,9 @@ def run(
         dnn=False,  # use OpenCV DNN for ONNX inference
         vid_stride=1,  # video frame-rate stride
 ):
+
+    save_one_box()
+    
     source = str(source)
     save_img = not nosave and not source.endswith('.txt')  # save inference images
     is_file = Path(source).suffix[1:] in (IMG_FORMATS + VID_FORMATS)
